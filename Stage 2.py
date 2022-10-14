5707302407f0bcf722810a47d51a0b5aac0f9a31
dimitri lanier <dimlanier@gmail.com>
2022-10-14 20:19:49 +0200

    c3

--- Stage 2.py
+++ Stage 2.py
@@ -177,16 +177,17 @@ def detect_reccurenceV2(database) :
 
 
     #Pour chaque ligne (entreprise) on regarde si elle triche en procedant comme suit :
+    #-Pour chacune des entreprise on commence par trouver la premiere date ( correspondant )
     for i in range(0, nb_of_ids) :
-        index_1 = 0 #Index de la premiere transaction d'une suite de transaction suspecte
+        index_1 = 0 #Index de la première transaction d'une suite de transaction suspecte
         cheating = False
-        while index_1 < max_date and not cheating : #Pour chaque suite de transaction possible 
+        while index_1 < max_date and not cheating : #Pour chaque suites de transactions possibles 
             while index_1 < max_date and not data[i][index_1] : #On récupère l'index de la premiere transaction de la suite
                 index_1 += 1
             
             next_index = index_1 + 1
             index_2 = next_index
-            first = True #Permet de connetre l'index du début de la prochaine suite de transation suspecte
+            first = True #Permet de connaitre l'index du début de la prochaine suite de transation suspecte
             while 2 * index_2 - index_1 < max_date and not cheating : #Tant qu'il existe une suite de transaction suspecte 
                 while 2 * index_2 - index_1 < max_date and not data[i][index_2] : #On va chercher la deuxième transaction de la suite
                     index_2 += 1
